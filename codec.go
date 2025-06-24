594c8fb6bf8becbf17249e5ca244edd0e0dea5ca
Andrew Brown <ab36245@gmail.com>
2025-06-24 17:54:33 +1000

    Removed type defs

--- codec.go
+++ codec.go
@@ -2,12 +2,7 @@ package db
 
 import "go.mongodb.org/mongo-driver/v2/bson"
 
-type A = bson.A
-type D = bson.D
-type E = bson.E
-type M = bson.M
-
 type Codec[T any] struct {
-	Decode func(M) (T, error)
-	Encode func(T) (M, error)
+	Decode func(bson.M) (T, error)
+	Encode func(T) (bson.M, error)
 }
